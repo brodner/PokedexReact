bfc842a0f2f2a3c773f787c6151bc04b38f6e6f1
brodner <brodner.rocael.m.a@gmail.com>
2021-06-21 13:11:44 -0600

    Add: Feature search pokemon and pokemon info estast

--- src/components/Buscador.js
+++ src/components/Buscador.js
@@ -1,24 +1,30 @@
-import React from 'react'
-
+import React, {useState} from 'react';
+import {useHistory} from "react-router-dom";
 const Buscador = () => {
+    const [text, setText] = useState("")
+    const history = useHistory()
+    const textSearchPokemon = e => {
+        setText(e.target.value)
+        console.log(e.target.value);
+    }
+    const SearchPokemon = e => {
+        e.preventDefault()
+        history.push({path: `/PokemonInfo/${text}`})
+        history.replace(`/PokemonInfo/${text}`)
+    }
     return (
-        <div>
+        <form onSubmit={SearchPokemon}>
             <div className=" bg-white flex justify-center p-3 shadow-sm border border-gray-200 rounded-full">
                 <button className="outline-none focus:outline-none ">
-                    <svg className=" w-5 text-gray-600 h-5 cursor-pointer" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                    <svg className=" w-5 text-gray-600 h-5 cursor-pointer" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" stroke="currentColor" viewBox="0 0 24 24">
+                        <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                    </svg>
                 </button>
-                <input type="search" name="" id="" placeholder="Buscar Pokemon, Id, Nombre " x-model="q"
-                    className="w-full pl-4 text-sm text-gray-800 outline-none focus:outline-none bg-transparent" />
-                <div className="select">
-                    <select name="" id="" x-model="image_type" className="text-sm text-gray-800 outline-none focus:outline-none bg-transparent">
-                        <option value="all" defaultValue>All</option>
-                        <option value="photo">Photo</option>
-                        <option value="illustration">Illustration</option>
-                        <option value="vector">Vector</option>
-                    </select>
-                </div>
-            </div >
-        </div>
+                <input type="search" name="" id="" placeholder="Buscar Pokemon, Id, Nombre " x-model="q" className="w-full pl-4 text-sm text-gray-800 outline-none focus:outline-none bg-transparent"
+                    onChange={textSearchPokemon}
+                    value={text}/>
+            </div>
+        </form>
     )
 }
 
--- src/pages/PokemonInfo.js
+++ src/pages/PokemonInfo.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import Nav from './Nav'
-import { useParams } from "react-router-dom";
+import {useParams} from "react-router-dom";
 
 const backgroundColorTypePokemon = {
     ghost: "bg-violet-400",
@@ -20,10 +20,10 @@ const backgroundColorTypePokemon = {
     grass: "bg-emerald-300",
     bug: "bg-emerald-200",
     ground: "bg-yellow-500",
-    fairy: "bg-pink-200",
+    fairy: "bg-pink-200"
 }
 const PokemonInfo = (props) => {
-    const { pokemon } = useParams();
+    const {pokemon} = useParams();
     const [dataPokemon, setDataPokemon] = useState({})
     const [Load, setLoad] = useState(true)
     const [param, setParam] = useState(pokemon)
@@ -37,124 +37,129 @@ const PokemonInfo = (props) => {
             console.log(dataPokemon);
         } catch (error) {
             console.log(error);
+            console.log({dataPokemon});
         }
 
     }
     useEffect(() => {
         loadPokemonFromServer(param)
-        return () => {
-        }
+        return() => {}
     }, [param])
-
-
 
     return (
         <div className='font-body text-gray-700'>
             <Nav></Nav>
             {
-                Load ? (
-                    <div className="p-4 md:w-1/3">
-                        <div className="h-min w-min border-2 border-gray-200 rounded-lg overflow-hidden">
-                            <div className="lg:h-48 bg-gray-400 md:h-36 w-full object-cover object-center"></div>
-                            <div className="p-6">
-                                <h2 className="bg-gray-400 animate-pulse h-4 w-1/4 mb-2"> </h2>
-                                <h1 className="w-1/2 mb-4 h-6 animate-pulse bg-gray-500"> </h1>
-                                <p className="leading-relaxed mb-3 w-full h-3 animate-pulse bg-gray-400"></p>
-                                <p className="leading-relaxed mb-3 w-2/3 h-3 animate-pulse bg-gray-400"></p>
-                                <p className="leading-relaxed mb-3 w-1/2 h-3 animate-pulse bg-gray-400"></p>
-                                <div className="flex items-center flex-wrap ">
-                                    <p className="bg-indigo-300 h-4 animate-pulse mt-2 w-32 inline-flex items-center md:mb-2 lg:mb-0">
-                                    </p>
-                                    <span className="bg-indigo-300 w-16 mt-2 h-4 animate-pulse mr-3 px-2 inline-flex items-center ml-auto leading-none text-sm pr-5 py-1">
+                
+            }
+            {
+            Load ? (
+                <div className="p-4 md:w-1/3">
+                    <div className="h-min w-min border-2 border-gray-200 rounded-lg overflow-hidden">
+                        <div className="lg:h-48 bg-gray-400 md:h-36 w-full object-cover object-center"></div>
+                        <div className="p-6">
+                            <h2 className="bg-gray-400 animate-pulse h-4 w-1/4 mb-2"> </h2>
+                            <h1 className="w-1/2 mb-4 h-6 animate-pulse bg-gray-500"> </h1>
+                            <p className="leading-relaxed mb-3 w-full h-3 animate-pulse bg-gray-400"></p>
+                            <p className="leading-relaxed mb-3 w-2/3 h-3 animate-pulse bg-gray-400"></p>
+                            <p className="leading-relaxed mb-3 w-1/2 h-3 animate-pulse bg-gray-400"></p>
+                            <div className="flex items-center flex-wrap ">
+                                <p className="bg-indigo-300 h-4 animate-pulse mt-2 w-32 inline-flex items-center md:mb-2 lg:mb-0"></p>
+                                <span className="bg-indigo-300 w-16 mt-2 h-4 animate-pulse mr-3 px-2 inline-flex items-center ml-auto leading-none text-sm pr-5 py-1"></span>
 
-                                    </span>
-
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ) : (
+                <div className={
+                    `grid grid-cols-1 md:mx-15 pt-4 shadow-inner ${
+                        backgroundColorTypePokemon[dataPokemon.types[0].type.name]
+                    } `
+                }>
+                    <div className="col-span-1 flex flex-col items-center">
+                        {
+                        dataPokemon.id > 472 ? <img src={
+                                dataPokemon.sprites.other["official-artwork"].front_default
+                            }
+                            alt={
+                                dataPokemon.name
+                            }
+                            className=" object-cover w-max"/> : <img src={
+                                `https://img.pokemondb.net/sprites/go/normal/${
+                                    dataPokemon.name
+                                }.png`
+                            }
+                            alt=""
+                            className=" object-center mt-3 w-max"/>
+                        }
+                     </div>
+                    <div className="bg-white col-span-1 md:mx-16 border shadow-lg rounded-3xl pt-5">
+                        <h1 className='font-bold text-4xl px-5 py-3 text-center uppercase '>
+                            {
+                            dataPokemon.name
+                        }</h1>
+                        <div className="flex flex-wrap gap-1 col-span-1 justify-center">
+                            {
+                            dataPokemon.types.map(({
+                                type
+                            }, index) => <span key={
+                                    `type${index}`
+                                }
+                                className={
+                                    `block p-1 px-2 shadow-inner rounded-full bg-white font-extrabold capitalize mt-2 ${
+                                        backgroundColorTypePokemon[type.name]
+                                    }`
+                            }>
+                                {
+                                type.name
+                            }</span>)
+                        } </div>
+                        <div className="grid grid-cols-3 gap-2 px-4 mt-4">
+                            <div className="flex flex-col gap-4 border-2 rounded-lg border-gray-200 border-opacity-50 p-8 col-span-3 md:col-span-1">
+                                <h1 className="text-center text-sm">Estadisticas</h1>
+                                {
+                                dataPokemon.stats.map((data) => (
+                                    <div>
+                                        <div className="bg-white rounded-lg w-72 shadow block p-4 m-auto">
+                                            {
+                                            data.stat.name
+                                        }
+                                            <div className="w-full h-4 bg-gray-400 rounded-full mt-3">
+                                                <div className="h-full text-center text-xs text-white bg-red-500 rounded-full w-2/4 w-">
+                                                    {
+                                                    data.base_stat
+                                                }%
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))
+                            } </div>
+                            <div className="flex flex-wrap gap-40 px-8 justify-center border-2 rounded-lg border-gray-200 border-opacity-50 p-8  col-span-3 md:col-span-1">
+                                <div className="text-sm text-center border-b-2 border-red-500">
+                                    <span className='block'>No.</span>
+                                    <span className='block'>
+                                        {
+                                        dataPokemon.id
+                                    }</span>
+                                </div>
+                            </div>
+                            <div className="grid grid-cols-1 gap-4 px-8 justify-center border-2 rounded-lg border-gray-200 border-opacity-50 p-8  col-span-3 md:col-span-1">
+                                <h1 className="text-center text-sm col-span-1">Evoluciones</h1>
+                                <div className="text-sm text-center border-b-2 border-red-500 col-span-1">
+                                    <span className='block'>No.</span>
+                                    <span className='block'>
+                                        {
+                                        dataPokemon.id
+                                    }</span>
                                 </div>
                             </div>
                         </div>
                     </div>
-                )
-                    :
-
-                    (<div className={`grid grid-cols-1 md:mx-15 pt-4 shadow-inner ${backgroundColorTypePokemon[dataPokemon.types[0].type.name]} `}>
-                        <div className="col-span-1 flex flex-col items-center">
-                            {
-                                dataPokemon.id > 472 ?
-                                    <img src={dataPokemon.sprites.other["official-artwork"].front_default}
-                                        alt={dataPokemon.name}
-                                        className=" object-cover w-max" />
-                                    :
-                                    <img src={`https://img.pokemondb.net/sprites/go/normal/${dataPokemon.name}.png`} alt="" className=" object-center mt-3 w-max" />
-                            }
-                        </div>
-                        <div className="bg-white col-span-1 md:mx-16 border shadow-lg rounded-3xl pt-5">
-                            <h1 className='font-bold text-4xl px-5 py-3 text-center uppercase '>{dataPokemon.name}</h1>
-                            <div className="flex flex-wrap gap-1 col-span-1 justify-center">
-                                {
-                                    dataPokemon.types.map(({ type }, index) => <span key={`type${index}`} className={`block p-1 px-2 shadow-inner rounded-full bg-white font-extrabold capitalize mt-2 ${backgroundColorTypePokemon[type.name]}`}>{type.name}</span>)
-                                }
-                            </div>
-                            <div class="flex flex-col gap-4">
-                                <div>
-                                    <div class="bg-white rounded-lg w-72 shadow block p-4 m-auto">
-                                        <div class="w-full h-4 bg-gray-400 rounded-full mt-3">
-                                            <div class="w-3/4 h-full text-center text-xs text-white bg-red-500 rounded-full">
-                                                75%
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div>
-                                    <div class="bg-white rounded-lg w-72 shadow block p-4 m-auto">
-                                        <div class="w-full h-4 bg-gray-400 rounded-full mt-3">
-                                            <div class="w-3/4 h-full text-center text-xs text-white bg-yellow-500 rounded-full">
-                                                75%
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div>
-                                    <div class="bg-white rounded-lg w-72 shadow block p-4 m-auto">
-                                        
-                                        <div class="w-full h-4 bg-gray-400 rounded-full mt-3">
-                                            <div class="w-3/4 h-full text-center text-xs text-white bg-indigo-500 rounded-full">
-                                                75%
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div>
-                                    <div class="bg-white rounded-lg w-72 shadow block p-4 m-auto">
-                                        <div class="w-full h-4 bg-gray-400 rounded-full mt-3">
-                                            <div class="w-3/4 h-full text-center text-xs text-white bg-pink-500 rounded-full">
-                                                75%
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="flex flex-wrap gap-40 px-8 justify-center">
-                                <div className="text-4xl text-center">
-                                    <span className='block'>No.</span>
-                                    <span className='block'>006</span>
-                                </div>
-                                <div className="text-4xl text-center">
-                                    <span className='block'>LEVEL</span>
-                                    <span className='block'>006</span>
-                                </div>
-                                <div className="text-4xl text-center">
-                                    <span className='block'>TYPE</span>
-                                    <span className='block'>006</span>
-                                </div>
-                                <div className="text-4xl text-center">
-                                    <span className='block'>No.</span>
-                                    <span className='block'>006</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>)
-            }
-        </div>
+                </div>
+            )
+        }</div>
     )
 }
 
